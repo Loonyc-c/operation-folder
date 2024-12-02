e426789d5232b3c9f5fed8337c8b6407008bb45e
Battulga Batkhuyag <battulgabathuyg54@gmail.com>
2024-12-03 01:31:13 +0800

    aa

--- index.js
+++ index.js
@@ -1,19 +1,72 @@
-const number=1
-const stringNumber="1"
+// 8. Гурвалжингийн периметрийг олох
+const triangleA = 10;
+const triangleB = 10;
+const trianglec = 10;
+const result = triangleA + triangleB + trianglec;
+console.log(result);
+// 9. Тойргийн талбайг олох
+const r = 7;
+const d = 14;
+// 10. m = y2-y1/x2-x1 хариуг олох
+const y = 2;
+const x = 3;
+const result10 = (y * 2 - y * 1) / (x * 2 - x * 1);
+console.log(result10);
+// 11. 15 МB дата 3 секундэд манай интернэт онлайнаар татаж чаддаг бол түүнийг BIT Rate-ийг олно уу.
+const mbData = 15;
+const time = 3;
+const result11 = 15 / 3;
+console.log(result11);
+// 12. side1, side2, side3 гэсэн хувьсагчдад гурвалжны гурван талыг онооно. Ийнхүү гурван тал өгөдсөн байхад үед гурвалжны талбайг ол.
+const side1 = 20;
+const side2 = 20;
+const side3 = 30;
+const result12 = (side1 * side3) / 2;
+console.log(result12);
+// 13. Өгсөн тоог цельсээс фаренгейт рүү хөрвүүл.
+const celsius = 25;
+const result13 = 25 * 1.8 + 32;
+console.log(result13);
+// 14. Хоёр багийн дундаж оноог олно уу. ( A team - 96, 108, 89; B team - 88, 91, 110 )
+const aTeam1 = 96;
+const aTeam2 = 108;
+const aTeam3 = 89;
+const aTeamResult = (aTeam1 + aTeam2 + aTeam3) / 3;
+console.log(aTeamResult);
+const bTeam1 = 88;
+const bTeam2 = 91;
+const bTeam3 = 110;
+const bTeamResult = (bTeam1 + bTeam2 + bTeam3) / 3;
+console.log(bTeamResult);
+// 15. 2 хувьсагч зарлан 2 тоон утга өг. Тухайн 2 хувьсагчдаар Math-ийн 5н үйлдэлүүдийг (+-*/%) гүцэтгээд үр дүнг нь дараах байдлаар харуул:
+// Өгөгдсөн 2 тооны нийлбэр нь 25
+// Өгөгдсөн 2 тооны ялгавар нь 5
+const a = 25;
+const b = 5;
+const aResult = (a + b) / 2;
+console.log(aResult);
+const bResult = a - aResult;
+console.log(bResult);
+const xx = 15;
+const yy = 10;
+const sum = xx + yy;
+const sub = xx - yy;
+const multiply = xx * yy;
+const divide = xx / yy;
+console.log(divide);
+const Remainder = xx % yy;
+console.log(Remainder);
 
-console.log (number === stringNumber);
-console.log (number == stringNumber);
+// 16. cm -г inch рүү хөрвүүлэх. inch -г cm рүү хөрвүүлэх
+const cm = 15;
+const inch = 20;
+const cmResult = cm / 2.54;
+console.log(cmResult);
+const inchResult = inch * 2.54;
+console.log(inchResult);
+// 17. Гараас дугуйн радиус буюу r орж ирэхэд тухайн дугуйн эзэлхүүнийг ол. Гараас дугуйн радиус буюу r, орж ирэхэд тухайн дугуйн хүрээний уртыг ол.
 
-// 1
-const triangleA= 10
-const triangleB= 10
-const trianglec= 10 
-console.log (triangleA + triangleB + trianglec)
+// 18. 3 оронтоо тооны хамгийн эхний цифрийг олох илэрхийлэл бич
 
-// 2
-const y=10
-const x=10
-console.log (y*2-y*1/x*2-x*1)
-
-// 3 
-
+const digit = 182;
+console.log(digit);
