e32e3c1000cf0d4dd65139d2fae9ba9f2a2852a1
battulga <battulgabathuyg54@gmail.com>
2024-12-02 17:56:14 +0800

    bodlog

---
+++ index.js
@@ -0,0 +1,19 @@
+const number=1
+const stringNumber="1"
+
+console.log (number === stringNumber);
+console.log (number == stringNumber);
+
+// 1
+const triangleA= 10
+const triangleB= 10
+const trianglec= 10 
+console.log (triangleA + triangleB + trianglec)
+
+// 2
+const y=10
+const x=10
+console.log (y*2-y*1/x*2-x*1)
+
+// 3 
+
